68645e26132632a06fb1aa390b811593682b3a7a
ahmetoguzlu <aoguzlu@berkeley.edu>
2018-10-04 18:39:07 -0700

    Card Prefab + MatAssign Func
    Created a simple card Prefab with realistic proportions. Implemented changeMaterialWithLoc to change the material attached to the card with code at any time during the game.
    TODO: Need more card textures

---
+++ Brain Games/Assets/CardMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMain : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		changeMaterialWithLoc("Number/DummyNum");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// Other Functions
+
+	void changeMaterialWithLoc(string local_address){
+		GetComponent<Renderer>().material = Resources.Load(local_address, typeof(Material)) as Material;
+	}
+}
